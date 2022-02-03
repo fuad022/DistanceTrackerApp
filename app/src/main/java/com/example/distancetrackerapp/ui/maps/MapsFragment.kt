9a889a01dd2a766ae17b12c9611d3f0fa8d7c82b
f.r.malikov <fuad.malikov@rabitabank.com>
2022-02-03 16:36:00 +0400

    Update and Observe location list.

--- app/src/main/java/com/example/distancetrackerapp/ui/maps/MapsFragment.kt
+++ app/src/main/java/com/example/distancetrackerapp/ui/maps/MapsFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -24,6 +25,7 @@ import com.example.distancetrackerapp.util.Permissions.requestBackgroundLocation
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 import com.vmadalin.easypermissions.EasyPermissions
 import com.vmadalin.easypermissions.dialogs.SettingsDialog
 import kotlinx.coroutines.delay
@@ -35,6 +37,8 @@ class MapsFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationButto
     private val binding get() = _binding!!
 
     private lateinit var map: GoogleMap
+
+    private var locationList = mutableListOf<LatLng>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -71,6 +75,16 @@ class MapsFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationButto
             isCompassEnabled = false
             isScrollGesturesEnabled = false
         }
+        observeTrackerService()
+    }
+
+    private fun observeTrackerService() {
+        TrackerService.locationList.observe(viewLifecycleOwner, {
+            if (it != null) {
+                locationList = it
+                Log.d("LocationList", locationList.toString())
+            }
+        })
     }
 
     private fun onStartButtonClicked() {
